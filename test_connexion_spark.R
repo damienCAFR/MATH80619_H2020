f212246c59abd1bfc5b3e5d6604a5347ccebaf3b
damienCAFR <damien.janvrin@gmail.com>
2020-02-22 15:01:26 -0500

    ajout commande spark

--- test_connexion_spark.R
+++ test_connexion_spark.R
@@ -1,13 +1,37 @@
 #install.packages("sparklyr")
 
-
-library(sparklyr)
-library(dplyr)
-
 Sys.setenv(HADOOP_CONF_DIR = '/etc/hadoop/conf')
 Sys.setenv(YARN_CONF_DIR = '/etc/hadoop/conf')
 Sys.setenv(SPARK_HOME="/usr/lib/spark")
 
+# SPARKLYR
+
+library(sparklyr)
+library(dplyr)
+
 sc <- spark_connect(master = "yarn-client")
 
 spark_read_csv(sc, name="test", path="gs://twitter_rstudio/fishharvest.csv")
+
+spark_read_csv(sc, name="test2", path="gs://twitter_rstudio/fishharvest.csv")
+
+test  <- spark_read_csv(sc, name="test2", path="gs://twitter_rstudio/fishharvest.csv")
+
+
+test_df <- test %>% 
+  group_by(species) %>%
+  summarise(nb_lignes=n()) %>% collect()
+
+
+# SPARKR
+
+library(SparkR)
+
+if (nchar(Sys.getenv("SPARK_HOME")) < 1) {
+  Sys.setenv(SPARK_HOME = "/usr/lib/spark")
+}
+library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
+sparkR.session(master = "yarn-client", sparkConfig = list(spark.driver.memory = "2g"))
+
+people <- read.df("gs://twitter_rstudio/fishharvest.csv", "csv")
+head(people)
